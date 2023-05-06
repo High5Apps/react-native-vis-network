c5950271dedb742d418c2cc36cc14d2fbb129bae
Julian Tigler <High5Apps@gmail.com>
2023-05-07 00:00:26 +0300

    chore: replace multiply example with VisNetwork placeholder

---
+++ src/VisNetwork.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function VisNetwork() {
+  // eslint-disable-next-line react-native/no-inline-styles
+  return <View style={{ backgroundColor: 'black', height: 250, width: 250 }} />;
+}
