e94949d59900cf70bc91c9a377d245d009056845
Julian Tigler <High5Apps@gmail.com>
2023-05-09 23:13:42 +0300

    feat: draw basic static network
    - Add peerDependency (dependency in example) on react-native-webview
      - https://github.com/react-native-webview/react-native-webview
    - Add internal WebView dependency on vis-network@9.1.6 via unpkg.com CDN
      - https://unpkg.com/browse/vis-network@9.1.6/

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,58 @@
 import * as React from 'react';
+import graph from './graph.json';
 
 import { StyleSheet, View } from 'react-native';
 import VisNetwork from 'react-native-vis-network';
 
+const accent = '#8944AB';
+
 export default function App() {
+  const { edges, nodes } = graph;
+  const options = {
+    edges: {
+      color: accent,
+      width: 2,
+    },
+    interaction: {
+      dragNodes: false,
+      dragView: false,
+      keyboard: false,
+      selectable: false,
+      zoomView: false,
+    },
+    layout: {
+      randomSeed: 12345,
+    },
+    nodes: {
+      color: accent,
+    },
+  };
   return (
-    <View style={styles.container}>
-      <VisNetwork />
+    <View style={styles.background}>
+      <View style={styles.container}>
+        <VisNetwork
+          containerStyle={styles.networkContainer}
+          edges={edges}
+          nodes={nodes}
+          options={options}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
+    backgroundColor: '#ccc',
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  container: {
+    aspectRatio: 1,
+  },
+  networkContainer: {
+    borderColor: accent,
+    borderWidth: 10,
   },
 });
--- src/VisNetwork.tsx
+++ src/VisNetwork.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
-import { View } from 'react-native';
+import type { ViewStyle } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function VisNetwork() {
-  // eslint-disable-next-line react-native/no-inline-styles
-  return <View style={{ backgroundColor: 'black', height: 250, width: 250 }} />;
+const VIS_NETWORK_VERSION = '9.1.6';
+
+type Props = {
+  containerStyle?: ViewStyle;
+  edges: { from: string; to: string }[];
+  nodes: { id: string }[];
+  options: any;
+  style?: ViewStyle;
+};
+
+export default function VisNetwork({
+  containerStyle,
+  edges,
+  nodes,
+  options,
+  style,
+}: Props) {
+  const html = `
+<!DOCTYPE html>
+<html>
+<head>
+  <script
+    type="text/javascript"
+    src="https://unpkg.com/vis-network@${VIS_NETWORK_VERSION}/standalone/umd/vis-network.min.js">
+  </script>
+</head>
+<body>
+<div id="container" style="height: 100vh;"></div>
+<script type="text/javascript">
+    const nodes = new vis.DataSet(${JSON.stringify(nodes)});
+    const edges = new vis.DataSet(${JSON.stringify(edges)});
+    const container = document.getElementById('container');
+    const graph = { edges, nodes };
+    const options = ${JSON.stringify(options)};
+    const network = new vis.Network(container, graph, options);
+    network.fit();
+</script>
+</body>
+</html>
+`;
+
+  return (
+    <WebView
+      containerStyle={containerStyle}
+      originWhitelist={['*']}
+      source={{ html }}
+      style={style}
+    />
+  );
 }
